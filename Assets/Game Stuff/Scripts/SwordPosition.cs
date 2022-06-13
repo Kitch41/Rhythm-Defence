ff403f3cd2ebb601a24e9e3bc18e9144f714d061
Youri Langenberg <youri.langenberg@hotmail.com>
2022-06-13 09:19:19 +0200

    Added movement to sword
    If we even wanna use that sword

--- Assets/Game Stuff/Scripts/SwordPosition.cs
+++ Assets/Game Stuff/Scripts/SwordPosition.cs
@@ -13,6 +13,18 @@ public class SwordPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Move the sword to a set of coordinates
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    transform.position = new Vector3(-28, 0, -296);
+                }
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    transform.position = new Vector3(0, 0, -296);
+                }
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    transform.position = new Vector3(30,0, -296);
+                }
     }
 }
